991806fbba99b508f5e94b3c67e53cbb9da62bf0
Iale Moreira <iale_moreira@educadventista.org>
2026-05-23 17:32:23 -0300

    Adiciona Firebase ao CORS

--- src/main/java/com/unasp/comanda_digital/config/SecurityConfig.java
+++ src/main/java/com/unasp/comanda_digital/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:62804"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:62804", "https://comanda-digital-cdc52.web.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
